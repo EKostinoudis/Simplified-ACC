5e86fbab5c2cc888a4fc0ca273eea8344280cf17
EKostinou <vagelis789@gmail.com>
2019-01-05 19:36:34 +0200

    Add some helper functions to iFilterbank

--- src/Level 1/iFilterbank.m
+++ src/Level 1/iFilterbank.m
@@ -24,7 +24,69 @@ function frameT = iFilterbank(frameF, frameType, winType)
 %
 % Returns:
 % - frameT:     The frame in the time domain.
-%
+%   
 
 end
 
+
+% Functions
+function W = KBD_left(N)
+M = N/2;
+
+if N == 2048 
+    a = 4;
+else
+    a = 6;
+end
+
+% M+1-point Kaiser window
+w = kaiser(M+1, a);
+
+% Cumulative sum of w
+cumw = cumsum(w);
+        
+%W = sqrt([cumw(1:end-1); cumw(end:-1:2)] ./ cumw(end));
+W = sqrt(cumw(1:end-1) ./ cumw(end));
+end
+
+function W = KBD_right(N)
+M = N/2;
+
+if N == 2048 
+    a = 4;
+else
+    a = 6;
+end
+
+% M+1-point Kaiser window
+w = kaiser(M+1, a);
+
+% Cumulative sum of w
+cumw = cumsum(w);
+        
+W = sqrt(cumw(end:-1:2) ./ cumw(end));
+end
+
+function W = sin_left(N)
+W = sin(pi./N * ((0:N/2-1) + 0.5));
+W = W(:); % make W column vector
+end
+
+function W = sin_right(N)
+W = sin(pi./N * ((N/2:N-1) + 0.5));
+W = W(:); % make W column vector
+end
+
+function s = IMDCT(X, N)
+% Spectral coefficient
+nSeq = 0:N-1;
+
+% init s
+s = zeros(N, 1);
+
+% Calculate s
+for n = nSeq
+    s(n + 1) = 2 / N * dot(X, cos((2 * pi / N) * (n + (N/2 + 1)/2 ) ...
+        * ((0:N/2-1) + 0.5)));
+end
+end
