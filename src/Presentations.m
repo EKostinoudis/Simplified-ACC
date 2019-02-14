c9688ce3dfc96cbcaef8d93d7344de1d9199c246
nicktheway <nikospa96@hotmail.gr>
2019-02-14 02:08:04 +0200

    Updated presentations to include demo3

--- src/Presentations.m
+++ src/Presentations.m
@@ -1,12 +1,19 @@
-%% Demo1 -- Data
+%% Init (Always run this section)
+% Input
 fNameIn = 'LicorDeCalandraca.wav';
-audioIn_1 = audioread(char(fNameIn));
+audioIn = audioread(char(fNameIn));
+
+% Shall the plots be saved? And where.
+save_files = 1;
+plotpath = './plots/';
+
+%% Demo1 -- Data
 AACSeq1 = AACoder1(fNameIn);
 audioOut_1 = iAACoder1(AACSeq1, 'demo1.wav');
 
 % Remove the frames that aren't overlapping in audioIn and audioOut
 audioOut_1 = audioOut_1(1025:end - 1024, :);
-audioIn_1 = audioIn_1(1025:length(audioOut_1) + 1024, :);
+audioIn_1 = audioIn(1025:length(audioOut_1) + 1024, :);
 
 % Calculate noise
 noise_1 = audioIn_1 - audioOut_1;
@@ -15,10 +22,6 @@ noise_1 = audioIn_1 - audioOut_1;
 SNR_1 = [snr(audioIn_1(:, 1), noise_1(:, 1)); snr(audioIn_1(:, 2), noise_1(:, 2))];
 
 %% Demo1 -- Plots (Requires: Demo1 -- Data)
-% Shall the plots be saved? And where.
-save_files = 1;
-plotpath = './plots/';
-
 % --- Error plot ---
 figure('name', 'Demo 1 - Error plot')
 plot(noise_1)
@@ -44,4 +47,95 @@ bar(frameTypes, frameTypeSums)
 title('Frame types in the sample')
 if save_files == 1
     print([plotpath 'Demo 1 - Frame Types'], '-dpng')
+end
+
+%% Demo2 -- Data
+AACSeq2 = AACoder2(fNameIn);
+audioOut_2 = iAACoder2(AACSeq2, 'demo2.wav');
+
+% Remove the frames that aren't overlapping in audioIn and audioOut
+audioOut_2 = audioOut_2(1025:end - 1024, :);
+audioIn_2 = audioIn(1025:length(audioOut_2) + 1024, :);
+
+% Calculate noise
+noise_2 = audioIn_2 - audioOut_2;
+
+% Calculate SNR
+SNR_2 = [snr(audioIn_2(:, 1), noise_2(:, 1)); snr(audioIn_2(:, 2), noise_2(:, 2))];
+
+%% Demo2 -- Plots (Requires: Demo2 -- Data)
+% --- Error plot ---
+figure('name', 'Demo 2 - Error plot')
+plot(noise_2)
+title('Error plot')
+legend(['Channel 1'; 'Channel 2'], 'Location', 'best')
+if save_files == 1
+    print([plotpath 'Demo 2 - Error plot'], '-dpng')
+end
+
+%% Demo1-Demo2 -- Plots (Requires: Demo1 -- Data AND Demo2 -- Data)
+% --- TNS diff ---
+frame = 51;
+% Apply iTNS
+frameFoutLeft = iTNS(AACSeq2(frame).chl.frameF, AACSeq2(frame).frameType, AACSeq2(frame).chl.TNScoeffs);
+frameFoutRight = iTNS(AACSeq2(frame).chr.frameF, AACSeq2(frame).frameType, AACSeq2(frame).chr.TNScoeffs);    
+
+% Construct frameF
+if AACSeq2(frame).frameType == "ESH"
+    frameF_demo2 = [reshape(frameFoutLeft, [1024, 1]), reshape(frameFoutRight, [1024, 1])];
+else
+    frameF_demo2 = [frameFoutLeft, frameFoutRight];
+end
+
+
+% Construct frameF of Demo1
+if AACSeq1(frame).frameType == "ESH"
+    frameF_demo1 = [reshape(AACSeq1(frame).chl.frameF, [1024, 1]), ...
+        reshape(AACSeq1(frame).chr.frameF, [1024, 1])];
+else
+    frameF_demo1 = [AACSeq1(frame).chl.frameF, AACSeq1(frame).chr.frameF];
+end
+
+% Maybe plot something.
+
+%% Demo3 -- Data (Use this if you do not have an updated "AACSeq3.mat" file)
+% !!!Warning: This section will need 3-30 minutes to complete depending on
+% the computer !!!
+AACSeq3 = AACoder3(fNameIn, 'AACSeq3.mat');
+audioOut_3 = iAACoder3(AACSeq3, 'demo3.wav');
+
+% Remove the frames that aren't overlapping in audioIn and audioOut
+audioOut_3 = audioOut_3(1025:end - 1024, :);
+audioIn_3 = audioIn(1025:length(audioOut_3) + 1024, :);
+
+% Calculate noise
+noise_3 = audioIn_3 - audioOut_3;
+
+% Calculate SNR
+SNR_3 = [snr(audioIn_3(:, 1), noise_3(:, 1)); snr(audioIn_3(:, 2), noise_3(:, 2))];
+
+%% Demo3 -- Data (Use this if you have an updated "AACSeq3.mat" file.)
+load AACSeq3
+audioOut_3 = iAACoder3(AACSeq3, 'demo3.wav');
+
+% Remove the frames that aren't overlapping in audioIn and audioOut
+audioOut_3 = audioOut_3(1025:end - 1024, :);
+audioIn_3 = audioIn(1025:length(audioOut_3) + 1024, :);
+
+% Calculate noise
+noise_3 = audioIn_3 - audioOut_3;
+
+% Calculate SNR
+SNR_3 = [snr(audioIn_3(:, 1), noise_3(:, 1)); snr(audioIn_3(:, 2), noise_3(:, 2))];
+
+%% Demo3 -- Plots (Requires: Demo3 -- Data)
+% --- Error plot ---
+figure('name', 'Demo 3 - Error plot')
+hold on
+plot(audioIn_3)
+plot(noise_3)
+title('Signal and noise plot')
+legend('Channel 1', 'Channel 2', 'Noise: Channel 1', 'Noise: Channel 2', 'Location', 'best')
+if save_files == 1
+    print([plotpath 'Demo 3 - Signal and noise plot'], '-dpng')
 end
